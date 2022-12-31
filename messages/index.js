ebb055a175eed328bb50a5913692ee9a31590690
Raktim Kashyap Das <raktimk16@gmail.com>
2022-12-31 16:37:59 +0530

    basic

--- messages/index.js
+++ messages/index.js
@@ -1,8 +1,11 @@
 export const newYear23 = (name) => {
-  return [
+  let wishes = [
     `Happy New Year ${name}! As we bid farewell to 2022 and welcome in 2023, I hope that the new year brings you joy, happiness, and all the things you wish for. May this be a year of new beginnings, growth, and abundance for you. Here's to a wonderful 2023!`,
     `Happy New Year ${name}! I hope 2023 brings you all the joy, prosperity, and success you desire. May the new year bring you new opportunities and experiences, and may you be surrounded by love and positivity. Here's to a wonderful year ahead!`,
     `Happy New Year ${name}! I hope that 2023 brings you joy, health, and prosperity. May all of your dreams and goals come true, and may you have the strength and determination to pursue them. Here's to a wonderful new year filled with endless possibilities! Wishing you all the best.`,
     `Happy New Year ${name}! As we ring in the new year, let's take a moment to reflect on the past and look forward to the future with hope and determination. Here's to a year filled with love, laughter, and all of the things that bring us joy. May 2023 be a year of growth, prosperity, and new opportunities for us all. Happy New Year!`,
   ];
+
+  let wishNumber = Math.floor(Math.random() * wishes.length);
+  return wishes[wishNumber];
 };
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,21 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { newYear23 } from "../messages";
+import classes from "../styles/Home.module.css";
 
 export default function Home() {
-  return <div></div>;
+  const { query } = useRouter();
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    let name = query?.name || "";
+    let wish = newYear23(name);
+    setMessage(wish);
+  }, [query]);
+
+  return (
+    <div className={classes.root}>
+      <div className={classes.messageBox}>{message}</div>
+    </div>
+  );
 }
