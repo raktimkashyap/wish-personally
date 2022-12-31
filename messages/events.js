0ba83c4748b54bfb4e77d6307ae093bc4112860b
Raktim Kashyap Das <raktimk16@gmail.com>
2022-12-31 17:17:01 +0530

    added bg image

---
+++ messages/events.js
@@ -0,0 +1,6 @@
+export const events = (name) => {
+  return {
+    newYear: `Happy New Year ${name}!`,
+    birthDay: `Happy Birth Day ${name}!`,
+  };
+};
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,33 @@
+/* eslint-disable @next/next/no-img-element */
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { newYear23 } from "../messages";
+import { events } from "../messages/events";
 import classes from "../styles/Home.module.css";
 
 export default function Home() {
   const { query } = useRouter();
+  const [name, setName] = useState("");
   const [message, setMessage] = useState("");
 
   useEffect(() => {
-    let name = query?.name || "";
+    let name = query?.to?.replace("_", " ") || "";
+    setName(name);
     let wish = newYear23(name);
     setMessage(wish);
   }, [query]);
 
   return (
     <div className={classes.root}>
-      <div className={classes.messageBox}>{message}</div>
+      <img
+        className={classes.wishBG}
+        alt=""
+        src="https://res.cloudinary.com/cloud-rkd/image/upload/v1672486628/wishes/Gold_And_Green_Modern_Happy_New_Year_Your_Story_nodwux.gif"
+      />
+      <div className={classes.messageBox}>
+        <div className={classes.wish}>{events(name).newYear}</div>
+        <div className={classes.message}>{message}</div>
+      </div>
     </div>
   );
 }
